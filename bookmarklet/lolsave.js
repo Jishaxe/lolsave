f500b503d89ad53ec10ef6c2d401ee14d811c144
Josh Lee <josh@joshbe.me>
2016-02-07 16:29:15 +0000

    Check for thread reply mode

--- bookmarklet/lolsave.js
+++ bookmarklet/lolsave.js
@@ -6,7 +6,13 @@ function hostname(url) {
 
 // Check we're on the right website
 if (hostname(document.location.href) === 'lolcow.farm') {
-  
+  // Check we're on reply mode
+
+  if (/https:\/\/lolcow.farm\/.+\/res\/\d+\.html/.test(document.location.href)) {
+
+  } else {
+    alert('You need to be on thread reply mode to use lolsave.')
+  }
 } else {
   alert('You need to be on lolcow.farm to use lolsave.');
 }
