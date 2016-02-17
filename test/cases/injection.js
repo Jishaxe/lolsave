700ce2f3d9ba6dd38e270e1f5d2927b2b04a7656
Josh Lee <josh@joshbe.me>
2016-02-17 07:45:51 +0000

    Fix on selecting thread in test

--- test/cases/injection.js
+++ test/cases/injection.js
@@ -2,9 +2,10 @@ module.exports = {
   'Check the bookmarklet is injected successfully' : function (browser) {
     browser.init()
     browser.click('a[href="/b/index.html"]')
-    browser.pause(3000)
-    browser.click('.post.op:nth-of-type(1) a[href^="\/b\/res\/"]')
-    browser.pause(3000)
+    browser.useXpath()
+    browser.resizeWindow(1200, 1200)
+    browser.click("//div[starts-with(@id, 'thread_')][1]/div[1]/p/a[3]")
+    browser.pause(10000)
     browser.end()
   }
 }
