f41f3b695cc3bf6384e995748f94d2fc7f16fdb1
Josh Lee <josh@joshbe.me>
2016-02-23 15:43:39 +0000

    Flesh out restoring test case

--- test/cases/restoring.js
+++ test/cases/restoring.js
@@ -2,7 +2,59 @@ var fs = require('fs')
 var lolsave = fs.readFileSync('lolsave.js').toString()
 var lolrestore = fs.readFileSync('lolrestore.js').toString()
 
+function randomInt(low, high) {
+  return Math.floor(Math.random() * (high - low) + low)
+}
+
+var saved_post = {
+  id: -1,
+  i: randomInt(1, 30)
+}
+
 module.exports = {
-  'Check lolsave restores your place on click' : function (browser) {
+  'Check lolrestore restores your place when clicked': function(browser) {
+    browser.url('https://lolcow.farm/b/res/72686.html')
+    browser.execute(lolsave)
+    browser.execute(
+      function(i) {
+        $post = jQuery(".post.reply:nth-of-type(" + i + ")")
+        $post.css({
+          "background-color": "red"
+        })
+        jQuery('html, body').animate({
+          scrollTop: $post.offset().top - $(window).height() / 2
+        }, 500)
+
+        return $post.attr('id')
+      }, [saved_post.i],
+      function(res) {
+        saved_post.id = res.value.match(/\d\d\d\d\d/)[0]
+      })
+
+    browser.click('#lolsave-popup-first-use a')
+    browser.expect.element('.lolsave-notice').to.be.present.before(10000)
+    browser.getCookie('lolsave-thread-72686', function(cookie) {
+      this.assert.equal(cookie.value, saved_post.id)
+    })
+
+    browser.pause(6000)
+    browser.execute(function() {
+      jQuery('html, body').animate({
+        scrollTop: 0
+      }, 500)
+    })
+
+    browser.execute(lolrestore)
+    browser.expect.element('.lolsave-notice').text.to.equal("lolrestored").before(1000)
+    browser.execute(function(i) {
+      $post = jQuery(".post.reply:nth-of-type(" + i + ")")
+
+      return $post == $(document.elementFromPoint(50, $(window).height() / 2)).closest(".post.reply").get()[0]
+    }, [saved_post.i],
+    function(result) {
+      this.assert.equal(result.value, true)
+    })
+
+    browser.end()
   },
 }
