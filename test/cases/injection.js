fcc6f7848bd70c709d5b584f3dfb28e488e51e0d
Josh Lee <josh@joshbe.me>
2016-02-23 15:16:31 +0000

    Streamline some of the tests

--- test/cases/injection.js
+++ test/cases/injection.js
@@ -4,23 +4,13 @@ var lolsave = fs.readFileSync('lolsave.js').toString()
 module.exports = {
   'Check the bookmarklet is injected successfully' : function (browser) {
     browser.init()
-    //browser.click('a[href="/b/index.html"]')
-    browser.url('https://lolcow.farm/b/res/71092.html')
+    browser.url('https://lolcow.farm/b/res/72686.html')
     browser.useXpath()
-    //browser.resizeWindow(1200, 1200)
-    //browser.click("//div[starts-with(@id, 'thread_')][1]/[@class='intro']/div[1]/p/a[3]")
     browser.useCss()
     browser.execute(lolsave)
     browser.expect.element("#lolsave-indicator").to.be.present.before(1000)
     browser.end()
   },
-  /*
-  'Check lolsave pops up an alert when not loaded on lolcow' : function (browser) {
-    browser.url('http://www.google.com')
-    browser.execute(lolsave)
-    browser.expect.element("#lolsave-popup-not-on-site").to.be.present.before(1000)
-    browser.end()
-  },*/
 
   'Check lolsave pops up an alert when not loaded on a thread' : function (browser) {
     browser.init()
@@ -34,11 +24,8 @@ module.exports = {
 
   'Check lolsave pops up an alert when it is the first use' : function (browser) {
     browser.init()
-    //browser.click('a[href="/b/index.html"]')
     browser.useXpath()
-    browser.url('https://lolcow.farm/b/res/71092.html')
-    //browser.resizeWindow(1200, 1200)
-    //browser.click("//div[starts-with(@id, 'thread_')][1]/[@class='intro']/div[1]/p/a[3]")
+    browser.url('https://lolcow.farm/b/res/72686.html')
     browser.useCss()
     browser.execute(lolsave)
     browser.expect.element("#lolsave-popup-first-use").to.be.present.before(1000)
--- test/cases/restoring.js
+++ test/cases/restoring.js
@@ -4,13 +4,5 @@ var lolrestore = fs.readFileSync('lolrestore.js').toString()
 
 module.exports = {
   'Check lolsave restores your place on click' : function (browser) {
-    browser.init()
-    browser.click('a[href="/b/index.html"]')
-    browser.useXpath()
-    browser.resizeWindow(1200, 1200)
-    browser.click("//div[starts-with(@id, 'thread_')][1]/div[1]/p/a[3]")
-    browser.useCss()
-    browser.execute(lolsave)
-    browser.end()
   },
 }
