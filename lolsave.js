2caa5f5f37985af9960fe742b0d2380d17658156
Josh Lee <josh@joshbe.me>
2016-02-25 15:31:43 +0000

    Experimenting with Jenkins

--- lolsave.js
+++ lolsave.js
@@ -7,7 +7,6 @@
 // Cookie manipulation stuff (from QuirksMode)
 function createCookie(name, value, days) {
   var expires;
-
   if (days) {
     var date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
